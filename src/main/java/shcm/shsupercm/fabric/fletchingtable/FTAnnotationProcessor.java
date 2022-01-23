553ce74b7ca3f464451f537820cd9e63180456e4
SHsuperCM <shsupercm@gmail.com>
2022-01-23 20:13:29 +0200

    Implemented annotation processor and built Entrypoint API

---
+++ src/main/java/shcm/shsupercm/fabric/fletchingtable/FTAnnotationProcessor.java
@@ -0,0 +1,68 @@
+package shcm.shsupercm.fabric.fletchingtable;
+
+import shcm.shsupercm.fabric.fletchingtable.api.Entrypoint;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Set;
+
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes({ "shcm.shsupercm.fabric.fletchingtable.api.Entrypoint.Repeated", "shcm.shsupercm.fabric.fletchingtable.api.Entrypoint" })
+public class FTAnnotationProcessor extends AbstractProcessor {
+    private Writer writer;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        try {
+            FileObject file = processingEnv.getFiler().createResource(StandardLocation.SOURCE_OUTPUT, "fletchingtable", "entrypoints.txt");
+            file.delete();
+            writer = file.openWriter();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        try {
+            for (Element element : roundEnv.getElementsAnnotatedWith(Entrypoint.class))
+                process(element, element.getAnnotation(Entrypoint.class).value());
+
+            for (Element element : roundEnv.getElementsAnnotatedWith(Entrypoint.Repeated.class))
+                for (Entrypoint entrypoint : element.getAnnotation(Entrypoint.Repeated.class).value())
+                    process(element, entrypoint.value());
+
+            writer.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public void process(Element element, String entrypoint) throws IOException {
+        switch (element.getKind()) {
+            case CLASS:
+                writer.append(element.toString());
+                break;
+            case METHOD:
+            case FIELD:
+                writer.append(element.getEnclosingElement().toString())
+                        .append("::")
+                        .append(element.getSimpleName());
+                break;
+            default:
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Got annotated @Entrypoint of unexpected type " + element.getKind() + "!");
+        }
+        writer.append(" ")
+                .append(entrypoint)
+                .append("\n");
+    }
+}
---
+++ src/main/java/shcm/shsupercm/fabric/fletchingtable/api/Entrypoint.java
@@ -0,0 +1,44 @@
+package shcm.shsupercm.fabric.fletchingtable.api;
+
+import java.lang.annotation.*;
+
+/**
+ * Automatically adds the annotated element to the requested entrypoint in fabric.mod.json
+ */
+@Repeatable(Entrypoint.Repeated.class)
+@Retention(RetentionPolicy.SOURCE)
+@Target( { ElementType.TYPE, ElementType.METHOD, ElementType.FIELD } )
+public @interface Entrypoint {
+    /**
+     * Used on elements implementing ModInitializer to run code when the game starts.
+     */
+    String MAIN = "main";
+    /**
+     * Used on elements implementing ClientModInitializer to run code when the physical client starts.
+     */
+    String CLIENT = "client";
+    /**
+     * Used on elements implementing DedicatedServerModInitializer to run code when the physical server starts.
+     */
+    String SERVER = "server";
+    /**
+     * Used on elements implementing PreLaunchEntryPoint to run code as soon as possible(right before the game starts).<br>
+     * Use with caution!
+     */
+    String PRE_LAUNCH = "preLaunch";
+
+    /**
+     * @return entrypoint identifier to register the annotated element to
+     */
+    String value();
+
+    /**
+     * @deprecated Do not use!
+     */
+    @Deprecated
+    @Retention(RetentionPolicy.SOURCE)
+    @Target( { ElementType.TYPE, ElementType.METHOD, ElementType.FIELD } )
+    @interface Repeated {
+        Entrypoint[] value();
+    }
+}
