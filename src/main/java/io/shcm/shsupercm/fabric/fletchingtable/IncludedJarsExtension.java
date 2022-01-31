b2242e61ebd3e4ed23d01c63b807ad1bfdb7733e
SHsuperCM <shsupercm@gmail.com>
2022-01-31 16:39:29 +0200

    Removed extractAll() from includedJars, now only from() is needed

--- src/main/java/io/shcm/shsupercm/fabric/fletchingtable/IncludedJarsExtension.java
+++ src/main/java/io/shcm/shsupercm/fabric/fletchingtable/IncludedJarsExtension.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
 
 import java.io.File;
@@ -21,11 +22,19 @@ public abstract class IncludedJarsExtension {
     private final FletchingTableExtension fletchingTableExtension;
     private final File includedJarsCache;
     private final DependencyHandler dependencies;
-    private final Configuration configuration;
+    private final ConfigurationContainer configurations;
 
     public IncludedJarsExtension(Project project, FletchingTableExtension fletchingTableExtension) {
         this.fletchingTableExtension = fletchingTableExtension;
-        includedJarsCache = new File(project.getProjectDir(), ".gradle/fletchingtable/includedJarsCache");
+
+        this.includedJarsCache = new File(project.getProjectDir(), ".gradle/fletchingtable/includedJarsCache");
+
+        if (includedJarsCache.exists()) {
+            for (File file : Objects.requireNonNull(includedJarsCache.listFiles()))
+                if (file.isFile())
+                    file.delete();
+        } else
+            includedJarsCache.mkdirs();
 
         project.getRepositories().flatDir(repository -> {
             repository.dirs(includedJarsCache);
@@ -35,39 +44,36 @@ public abstract class IncludedJarsExtension {
         });
 
         this.dependencies = project.getDependencies();
-        this.configuration = project.getConfigurations().create("includedJarsInternalConfiguration");
-        this.configuration.setTransitive(false);
+        this.configurations = project.getConfigurations();
     }
 
     public void from(String dependencyString) {
-        this.dependencies.add("includedJarsInternalConfiguration", dependencyString);
-    }
+        if (!fletchingTableExtension.getEnableIncludedJars().get())
+            return;
 
-    public void extractAll() {
-        if (includedJarsCache.exists()) {
-            for (File file : Objects.requireNonNull(includedJarsCache.listFiles()))
-                if (file.isFile())
-                    file.delete();
-        } else
-            includedJarsCache.mkdirs();
+        Configuration configuration = configurations.create("includedJarsInternalConfiguration");
+        configuration.setTransitive(false);
 
-        if (fletchingTableExtension.getEnableIncludedJars().get())
-            for (File parentJarFile : this.configuration.resolve())
-                try {
-                    try (ZipFile parentJarZip = new ZipFile(parentJarFile)) {
-                        ZipEntry modJsonEntry = parentJarZip.getEntry("fabric.mod.json");
-                        if (modJsonEntry != null) {
-                            JsonObject modJson = JsonParser.parseReader(new InputStreamReader(parentJarZip.getInputStream(modJsonEntry), StandardCharsets.UTF_8)).getAsJsonObject();
-                            JsonArray jars = modJson.getAsJsonArray("jars");
-                            if (jars != null)
-                                for (JsonElement jar : jars) {
-                                    String jarPath = jar.getAsJsonObject().get("file").getAsString();
-                                    Files.copy(parentJarZip.getInputStream(parentJarZip.getEntry(jarPath)), includedJarsCache.toPath().resolve(jarPath.substring(jarPath.lastIndexOf('/') + 1)), StandardCopyOption.REPLACE_EXISTING);
-                                }
-                        }
+        this.dependencies.add(configuration.getName(), dependencyString);
+
+        for (File parentJarFile : configuration.resolve())
+            try {
+                try (ZipFile parentJarZip = new ZipFile(parentJarFile)) {
+                    ZipEntry modJsonEntry = parentJarZip.getEntry("fabric.mod.json");
+                    if (modJsonEntry != null) {
+                        JsonObject modJson = JsonParser.parseReader(new InputStreamReader(parentJarZip.getInputStream(modJsonEntry), StandardCharsets.UTF_8)).getAsJsonObject();
+                        JsonArray jars = modJson.getAsJsonArray("jars");
+                        if (jars != null)
+                            for (JsonElement jar : jars) {
+                                String jarPath = jar.getAsJsonObject().get("file").getAsString();
+                                Files.copy(parentJarZip.getInputStream(parentJarZip.getEntry(jarPath)), includedJarsCache.toPath().resolve(jarPath.substring(jarPath.lastIndexOf('/') + 1)), StandardCopyOption.REPLACE_EXISTING);
+                            }
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        configurations.remove(configuration);
     }
 }
