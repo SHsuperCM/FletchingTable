e445f518a459c0cf44feebf185148bb8218084bd
SHsuperCM <shsupercm@gmail.com>
2022-03-05 20:08:30 +0200

    Fixed shut up drasil 18 for 1.18.2

--- shutupdrasil/src/main/java/shcm/shsupercm/fabric/shutupdrasil/mixin/ShutTheFuckUpDrasil.java
+++ shutupdrasil/src/main/java/shcm/shsupercm/fabric/shutupdrasil/mixin/ShutTheFuckUpDrasil.java
@@ -5,22 +5,18 @@ import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.RunArgs;
-import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.mixin.Final;
+import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
 public class ShutTheFuckUpDrasil {
-    @Shadow @Final private static Logger LOGGER;
-
     @Inject(method = "createUserApiService", cancellable = true, at = @At(value = "HEAD"))
     public void ohMyFuckingGlobShutTheFuckUpImRunningInADevelopmentEnvironment(YggdrasilAuthenticationService authService, RunArgs runArgs, CallbackInfoReturnable<UserApiService> cir) {
         if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
-            LOGGER.error("Failed to verify authentication");
+            LogManager.getLogger().error("Failed to verify authentication");
             cir.setReturnValue(UserApiService.OFFLINE);
         }
     }
